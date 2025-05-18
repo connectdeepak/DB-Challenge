0a449dadcfb3c65cf53adc7da66cb0b193c6cb88
connectdeepak <connectdeepak.kr@gmail.com>
2025-05-18 20:55:13 +0530

    code clean up

--- src/main/java/com/dws/challenge/service/AccountsService.java
+++ src/main/java/com/dws/challenge/service/AccountsService.java
@@ -44,7 +44,7 @@ public class AccountsService {
 	 */
 	public void transfer(String fromAccountId, String toAccountId, BigDecimal amount) {
 		
-		System.out.println(" Start transfer reqeust from " + fromAccountId + " to account " + toAccountId);
+		log.info("Start Tranfer money - {} from account- {} to account - {}" ,amount ,fromAccountId,toAccountId );
 
 		if (amount.compareTo(BigDecimal.ZERO) <= 0) {
 			throw new IllegalArgumentException("Transfer amount must be positive");
@@ -62,9 +62,10 @@ public class AccountsService {
 		}
 
 		/*
-		 * we need to take locks in a order .Else it will go in deadlock situation. for
+		 * we need to take locks in Same order .Else it will go in deadlock situation,for
 		 * example if 2 transfer request is coming in parallel - Request#1: transfer x
-		 * dollar from A/C1 to A/C2 Request#2 - transfer x dollar from A/C2 to A/C1
+		 * dollar from A/C1 to A/C2 
+		 * Request#2 - transfer y dollar from A/C2 to A/C1
 		 */
 		Object lock1 = fromAccountId.compareTo(toAccountId) < 0 ? fromAccount : toAccount;
 		Object lock2 = fromAccountId.compareTo(toAccountId) < 0 ? toAccount : fromAccount;
@@ -75,7 +76,6 @@ public class AccountsService {
 				if (fromAccount.getBalance().compareTo(amount) < 0) {
 					throw new IllegalArgumentException("Insufficient funds in source account");
 				}
-
 				fromAccount.setBalance(fromAccount.getBalance().subtract(amount));
 				toAccount.setBalance(toAccount.getBalance().add(amount));
 
@@ -84,19 +84,18 @@ public class AccountsService {
 
 		try {
 			notificationService.notifyAboutTransfer(fromAccount,
-					String.format("Debited %s from account %s", amount, toAccountId));
+					String.format("Debited %s from account %s", amount, fromAccountId));
 		} catch (Exception e) {
 			log.error("Failed to notify debit transaction", e);
 		}
 
 		try {
 			notificationService.notifyAboutTransfer(toAccount,
-					String.format("Credited %s to account %s", amount, fromAccountId));
+					String.format("Credited %s to account %s", amount, toAccountId));
 		} catch (Exception e) {
 			log.error("Failed to notify Credit transaction", e);
 		}
-		
-		System.out.println(" End transfer reqeust from " + fromAccountId + " to account " + toAccountId);
+		log.info("End Tranfer money - {} from account- {} to account - {}" ,amount ,fromAccountId,toAccountId );
 
 	}
 
